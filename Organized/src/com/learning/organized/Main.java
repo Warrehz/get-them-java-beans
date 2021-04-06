8a9d2cda7529750313945db7f1cf71be049c0654
warrehz <mical.juarez@gmail.com>
2021-04-06 00:11:13 -0500

    some conditional assignment

--- Organized/src/com/learning/organized/Main.java
+++ Organized/src/com/learning/organized/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("This is a test.");
+
+        ifTest(5, 2);
+        ifTest(3, 8);
+    }
+
+    public static void ifTest(int a, int b) {
+        System.out.println((a > b ? a : b) + " is the larger number.");
     }
 }
