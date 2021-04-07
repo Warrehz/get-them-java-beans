ce469ad3606b73bc2f87fdecd141b855147b0146
warrehz <mical.juarez@gmail.com>
2021-04-07 16:31:21 -0500

    simple error handling

--- CalcEngine/src/com/learning/calcengine/Main.java
+++ CalcEngine/src/com/learning/calcengine/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         double valueOne = 5;
-        double valueTwo = 5;
+        double valueTwo = 0;
         double result;
-        char opCode = 'a';
+        char opCode = 'd';
 
         if (opCode == 'a') {
             result = valueOne + valueTwo;
@@ -15,8 +15,14 @@ public class Main {
         } else if (opCode == 'm') {
             result = valueOne * valueTwo;
         } else if (opCode == 'd') {
-            result = valueOne / valueTwo;
+            if (valueTwo != 0) {
+                result = valueOne / valueTwo;
+            } else {
+                System.out.println("Cannot divide by zero.");
+                result = 0.00d;
+            }
         } else {
+            System.out.println("Invalid opCode: " + opCode);
             result = 0.00d;
         }
 
