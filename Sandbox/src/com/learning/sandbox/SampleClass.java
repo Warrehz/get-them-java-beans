b772597e06956f68227228309bf38ae022702ff3
warrehz <mical.juarez@gmail.com>
2021-04-16 23:58:08 -0500

    small update to sample class

--- Sandbox/src/com/learning/sandbox/SampleClass.java
+++ Sandbox/src/com/learning/sandbox/SampleClass.java
@@ -29,4 +29,11 @@ public class SampleClass {
         field2 = singleChar;
     }
 
+    public String[] getField3() {
+        return field3;
+    }
+
+    public void setField3(String[] field3) {
+        this.field3 = field3.clone();
+    }
 }
