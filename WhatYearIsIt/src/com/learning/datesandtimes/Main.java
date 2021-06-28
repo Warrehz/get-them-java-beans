45bb816a936ee90dc9ad38753468d5582788e8c6
warrehz <mical.juarez@gmail.com>
2021-06-27 23:53:26 -0500

    just simple time objects

--- WhatYearIsIt/src/com/learning/datesandtimes/Main.java
+++ WhatYearIsIt/src/com/learning/datesandtimes/Main.java
@@ -1,8 +1,30 @@
 package com.learning.datesandtimes;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZonedDateTime;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world.");
+
+        // current date in year-month-day format
+        LocalDate localDate = LocalDate.now();
+        System.out.println("Local Date: " + localDate);
+
+        // current time in hour:minute:second format
+        LocalTime localTime = LocalTime.now();
+        System.out.println("Local Time: " + localTime);
+
+        // current date & time in year-month-day:Separator:hour:minute:second format
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println("Local Date TIme: " + localDateTime);
+
+        // same as Local Date Time with addition of time zone
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println("Zoned Date Time: " + zonedDateTime);
+
+
     }
 }
